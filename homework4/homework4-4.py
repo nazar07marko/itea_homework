e3eaddd536b189371058fffe088e5112208556f8
nazar07marko <76966431+nazar07marko@users.noreply.github.com>
2021-08-31 18:21:29 +0300

    homework 4-4

--- homework4/homework4-4.py
+++ homework4/homework4-4.py
@@ -1,5 +1,12 @@
 a = {"salt": 2, "meat": 25, "apples": 6, "banana":3, "milk": 4.5, "bread": 2.5}
-i = 0
-while i < 3:
-    max(a.values())
-    i=i+1
+d = []
+h = 0
+while h < 3:
+    b = list(a.keys())[0]
+    for i in a:
+        if a[b] < a[i]:
+            b = i
+    d.append({b: a[b]})
+    a.pop(b) 
+    h=h+1
+print(d)
